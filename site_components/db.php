26205fef6f94b07aeac354bde6e51fc4ef27d3f0
ANDREY19992003 <57199854+ANDREY19992003@users.noreply.github.com>
2020-05-12 19:22:21 +0300

    Add files via upload

---
+++ site_components/db.php
@@ -0,0 +1,8 @@
+<?
+  $dbhost = "localhost";
+  $dbuser = "v91663tb_diction";
+  $dbpass = "Telefon00";
+  $dbname = "v91663tb_diction";
+  $mysqli = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+  $mysqli->set_charset("utf-8");
+?>
